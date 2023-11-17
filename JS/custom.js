3898d085741de31878fea842a66397077b2ae97b
thuyvtt50 <142789690+thuyvtt50@users.noreply.github.com>
2023-11-17 10:12:26 +0700

    Add files via upload

---
+++ JS/custom.js
@@ -0,0 +1,54 @@
+// JavaScript Document
+$('#banner').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+
+$('#intro-chay').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+
+$('#dichvu-chay').owlCarousel({
+    loop:true,
+    margin:30,
+    nav:true,
+	autoplay:true,
+	autoplayTimeout:5000,
+	smartSpeed:2000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
+    }
+})
